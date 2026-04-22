7b76ae7d094d53c8fc82778df64bc8e9db778903
polin-sv <polinasvetlay@icloud.com>
2026-04-22 17:33:00 +0300

    soft task03

--- Task03/logic.cpp
+++ Task03/logic.cpp
@@ -5,3 +5,33 @@
 // Необходимо разработать функцию (или программу), которая находит
 // количество строк, в которых положительных элементов больше 
 // чем всех остальных (отрицательных и нулевых).
+
+#include "logic.h"
+
+int count_rows_with_more_positive_values(int matrix[DEFAULT_SIZE][DEFAULT_SIZE], int n, int m) {
+	if (n <= 0 || m <= 0) {
+		return -1;
+	}
+
+	int positive = 0;
+	//int negative = 0;
+	int count = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			if (matrix[i][j] > 0) {
+				positive++;
+			}
+			/*else {
+				negative++;
+			}*/
+		}
+		if (positive > n - positive) {
+			count++;
+		}
+	}
+
+	return count;
+}
