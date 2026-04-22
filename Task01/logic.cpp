cf5098ff55aebbf865962c68dce91e59e686d2e4
polin-sv <polinasvetlay@icloud.com>
2026-04-22 16:55:27 +0300

    soft task01

--- Task01/logic.cpp
+++ Task01/logic.cpp
@@ -4,3 +4,30 @@
 // Дана математическая прямоугольная матрица размером N на M.
 // Необходимо разработать функцию (или программу), которая 
 // вычисляет среднее арифметическое ненулевых элементов матрицы.
+
+#include "logic.h"
+
+double calculate_arithmetical_mean_of_nonzero_elements(int matrix[DEFAULT_SIZE][DEFAULT_SIZE], int n, int m) {
+	if (n <= 0 || m <= 0) {
+		return -1;
+	}
+
+	double sum = 0;
+	double count = 0;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			if (matrix[i][j] != 0) {
+				sum += matrix[i][j];
+				count++;
+			}
+		}
+	}
+
+	if (count == 0) {
+		return 0;
+	}
+
+	return sum / count;
+}
