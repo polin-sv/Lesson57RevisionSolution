f69e69b62a59b6920fb5a7ae4efa3881521dd197
polin-sv <polinasvetlay@icloud.com>
2026-04-22 17:48:35 +0300

    t4

--- Task04/logic.cpp
+++ Task04/logic.cpp
@@ -5,3 +5,64 @@
 // Необходимо разработать функцию (или программу), которая 
 // высчитывает сумму элементов в тех столбцах заданной матрицы,
 // которые содержат хотя бы один экстремальный элемент.
+#include "logic.h"
+
+int find_max(int matrix[DEFAUL_SIZE][DEFAUL_SIZE], int n, int m) {
+
+	int max = matrix[0][0];
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			if (max < matrix[i][j]) {
+				max = matrix[i][j];
+			}
+		}
+	}
+
+	return max;
+}
+
+int find_min(int matrix[DEFAUL_SIZE][DEFAUL_SIZE], int n, int m) {
+	int min = matrix[0][0];
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+
+			if (min > matrix[i][j]) {
+				min = matrix[i][j];
+			}
+		}
+	}
+
+	return min;
+}
+
+int sum_elements_of_columns_with_extreme_value(int matrix[DEFAUL_SIZE][DEFAUL_SIZE], int n, int m) {
+	if (n <= 0 || m <= 0) {
+		return -1;
+	}
+
+	int max = find_max(matrix, n, m);
+	int min = find_min(matrix, n, m);
+	int sum = 0;
+	int current_sum = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			current_sum += matrix[i][j];
+			if (matrix[i][j] == max
+				|| matrix[i][j] == min) {
+				sum += current_sum;
+			}
+			
+		}
+	}
+
+	return sum;
+}
